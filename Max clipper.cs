02fbc0782291739ae04ed3bf487d07b03908d7e1
Max <Max@PCMAX>
2023-09-26 13:31:19 +0200

    Add project files.

---
+++ Max clipper.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Concurrent;
+using System.Security.Cryptography;
+using System.Security.AccessControl;
+using System.Windows.Forms.VisualStyles;
+using System.Runtime.InteropServices.ComTypes;
+using MaxClipper;
+using Emgu.CV;
+using System.Text.RegularExpressions;
+using Emgu.CV.Structure;
+using Emgu.CV.XImgproc;
+using System.Reflection;
+using System.Drawing.Imaging;
+using System.CodeDom;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Emgu.CV.VideoStab;
+
+namespace MaxClipper //192; 200; 225
+{
+
+    public partial class MainWindow : Form
+    {
+
+        public string currentDir = System.IO.Directory.GetCurrentDirectory();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        Dictionary<string, float> segmentFrames = new Dictionary<string, float>();
+        private void handleScroll(float value, string direction)
+        {
+            if (videoObj == null) { return; }
+            segmentFrames[direction] = (float)(value * (1 / videoObj.frameRate));
+            //float remappedVal = value.Remap(hScrollBar1.Minimum, hScrollBar1.Maximum, 0, 1);
+            //int frameToGrab = (int)Math.Floor(videoObj.frameCount * remappedVal);
+            var frameToShow = videoObj.GetFrame((int)Math.Floor(value));
+            if(frameToShow == null) { return; }
+            pictureBox1.Image = frameToShow;
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            handleScroll(hScrollBar1.Value, "start");
+
+        }
+
+        private void hScrollBar2_Scroll(object sender, EventArgs e)
+        {
+            handleScroll(hScrollBar2.Value, "end");
+        }
+        string videoDir = null;
+
+        private void explorerButton(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath.Text = openFileDialog1.FileName;
+                videoDir = openFileDialog1.FileName;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void filePathLabel(object sender, EventArgs e)
+        {
+
+        }
+        VideoTool videoObj = null;
+        private void openFileButton(object sender, EventArgs e)
+        {
+            if (videoDir == null || !videoDir.EndsWith(".mp4") || !File.Exists(videoDir))
+            {
+                LogToConsole("ERROR: file is not a valid video file (only mp4 accepted.)");
+                return;
+            }
+            videoObj = new VideoTool(videoDir);
+            hScrollBar1.Maximum = videoObj.frameCount;
+            hScrollBar2.Maximum = videoObj.frameCount;
+            segmentFrames["start"] = 0;
+            segmentFrames["end"] = (float)(videoObj.frameCount * videoObj.frameRate);
+            LogToConsole("Frame count: " + videoObj.frameCount.ToString());
+            LogToConsole("Framerate: " + videoObj.frameRate.ToString());
+            pictureBox1.Image = videoObj.GetFrame(0);
+        }
+
+        int MaxLines = 18;
+        public void LogToConsole(string message)
+        {
+            consoleBox.AppendText(message + Environment.NewLine);
+            if (consoleBox.Lines.Length > MaxLines)
+            {
+                consoleBox.Lines = consoleBox.Lines.Skip(1).ToArray();
+            }
+            consoleBox.SelectionStart = consoleBox.Text.Length;
+            consoleBox.ScrollToCaret();
+        }
+
+        private void consoleLogger(object sender, EventArgs e)
+        {
+
+        }
+
+        public void InvokeConsoleLog(string message) //used to use console box from thread other than main
+        {
+            consoleBox.Invoke(new Action(() => LogToConsole(message)));
+        }
+
+        private void MainLoad(object sender, EventArgs e)
+        {
+            consoleBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void clipButton(object sender, EventArgs e)
+        {
+            if (videoObj == null)
+            {
+                LogToConsole("No video loaded!");
+                return;
+            }
+            if (segmentFrames == null)
+            {
+                LogToConsole("Internal error: segmentFrames is null. Try moving both scroll bars again.");
+                return;
+            }
+            LogToConsole(segmentFrames["start"].ToString() + " " +  segmentFrames["end"].ToString());
+            string clippedVideo = VideoTool.FFmpegUtils.extract_clip(videoDir, segmentFrames);
+        }
+        public string outputFolder = "./";
+        private void setOutputFolder_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                LogToConsole("Selected output directory: \"" + dialog.FileName + "\"");
+                outputFolder = dialog.FileName;
+            }
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+
+        public static float Remap(this float value, float from1, float to1, float from2, float to2)
+        {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
+
+    }
+
+
+    class VideoTool
+    {
+        public VideoCapture video;
+        public int frameCount;
+        public double frameRate;
+        public string videoName;
+        private int lastFrame = 0;
+        public VideoTool(string videoPath)
+        {
+            this.video = new VideoCapture(videoPath);
+            this.frameCount = GetFrameCount();
+            this.frameRate = GetFrameRate();
+            this.videoName = Path.GetFileNameWithoutExtension(videoPath);
+        }
+
+        public Bitmap GetFrame(int frameIndex)
+        {
+            this.video.Set(Emgu.CV.CvEnum.CapProp.PosFrames, frameIndex);
+            Mat matImage = video.QuerySmallFrame();
+            if(matImage == null) { return null; }
+            Bitmap frame = matImage.ToImage<Bgr, byte>().AsBitmap<Bgr, byte>();
+            return frame;
+        }
+
+
+        public int GetFrameCount()
+        {
+            return (int)this.video.Get(Emgu.CV.CvEnum.CapProp.FrameCount);
+        }
+
+        public double GetFrameRate()
+        {
+            return this.video.Get(Emgu.CV.CvEnum.CapProp.Fps);
+        }
+
+        [Obsolete("this is catastrophyically bad and unbelievably slow, do not use.", true)]
+        class VideoCache
+        {
+            private Dictionary<int, string> cache = new Dictionary<int, string>();
+            private string cachePath = null;
+            public VideoCache(VideoCapture videocap, int totalframes, string name)
+            {
+                cachePath = $".\\cache\\{name}_{new Random().Next(0, 1000000)}";
+                if (!File.Exists(cachePath))
+                {
+                    Directory.CreateDirectory($"{cachePath}");
+                }
+                for (int i = 0; i < totalframes; i++)
+                {
+                    if (!videocap.Set(Emgu.CV.CvEnum.CapProp.PosFrames, i)) { continue; }
+                    Mat frame = videocap.QueryFrame();
+                    if (frame == null) { continue; }
+                    Console.WriteLine($"{cachePath}/{name}_{i}.jpeg");
+                    Emgu.CV.CvInvoke.Resize(frame, frame, new Size(768, 480), 0, 0, Emgu.CV.CvEnum.Inter.Linear);
+                    frame.Save($"{cachePath}\\{name}_{i}.jpeg");
+                }
+            }
+
+        }
+
+        public static class FFmpegUtils
+        {
+            public static string extract_clip(string videoDir, Dictionary<string, float> segment)
+            {
+                var start = segment["start"].ToString().Replace(",", ".");
+                var end = segment["end"].ToString().Replace(",", ".");
+                int counter = 0;
+                string outDir = null;
+                while (File.Exists(outDir = $"{Path.GetPathRoot(videoDir)}\\{Path.GetFileNameWithoutExtension(videoDir)}(clip {counter}).mp4"))
+                {
+                    counter++;
+                }
+                var proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "ffmpeg",
+                        Arguments = $"-ss {start} -i \"{videoDir}\" -c copy -t {end} \"{outDir}\"",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                List<string> output = new List<string>();
+                proc.Start();
+                while (!proc.StandardOutput.EndOfStream)
+                {
+                    string line = proc.StandardOutput.ReadLine();
+                    output.Add(line);
+                    // do something with line
+                }
+                proc.WaitForExit();
+                foreach(string line in output)
+                {
+                    Console.WriteLine(line);
+                }
+                return outDir;
+            }
+        }
+    }
+}
---
+++ Max clipper.designer.cs
@@ -0,0 +1,209 @@
+﻿namespace MaxClipper
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.consoleBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(109, 28);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(620, 20);
+            this.filePath.TabIndex = 1;
+            this.filePath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Video file path:";
+            this.label1.Click += new System.EventHandler(this.filePathLabel);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(586, 377);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFile
+            // 
+            this.openFile.Location = new System.Drawing.Point(771, 25);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(75, 23);
+            this.openFile.TabIndex = 4;
+            this.openFile.Text = "Open";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFileButton);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Location = new System.Drawing.Point(735, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 24);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.explorerButton);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.hScrollBar1.LargeChange = 1;
+            this.hScrollBar1.Location = new System.Drawing.Point(29, 499);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(817, 17);
+            this.hScrollBar1.TabIndex = 6;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.hScrollBar2.LargeChange = 1;
+            this.hScrollBar2.Location = new System.Drawing.Point(29, 530);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.hScrollBar2.Size = new System.Drawing.Size(817, 17);
+            this.hScrollBar2.TabIndex = 7;
+            this.hScrollBar2.Value = 100;
+            this.hScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(493, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 8;
+            // 
+            // consoleBox
+            // 
+            this.consoleBox.BackColor = System.Drawing.Color.Black;
+            this.consoleBox.ForeColor = System.Drawing.Color.White;
+            this.consoleBox.Location = new System.Drawing.Point(634, 80);
+            this.consoleBox.Name = "consoleBox";
+            this.consoleBox.ReadOnly = true;
+            this.consoleBox.Size = new System.Drawing.Size(212, 377);
+            this.consoleBox.TabIndex = 9;
+            this.consoleBox.Text = "";
+            this.consoleBox.TextChanged += new System.EventHandler(this.consoleLogger);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(771, 566);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Save clip";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.clipButton);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(672, 566);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Set output folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.setOutputFolder_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(200)))), ((int)(((byte)(225)))));
+            this.ClientSize = new System.Drawing.Size(883, 612);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.consoleBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePath);
+            this.Name = "MainWindow";
+            this.Text = "Max clipper";
+            this.Load += new System.EventHandler(this.MainLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox consoleBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
